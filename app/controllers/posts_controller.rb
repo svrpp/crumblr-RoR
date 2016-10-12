620d3098f76c146d7786577d6000062d6063299b
Angelica <samantha_vipparthi@yahoo.com>
2016-10-11 20:59:54 -0500

    Added support for images

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -58,5 +58,5 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :content)
+      params.require(:post).permit(:title, :kind, :content)
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161005024418) do
+ActiveRecord::Schema.define(version: 20161012013900) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "kind"
   end
 
 end
